a0fcef7d6f3f282b2d00e71726c586bd8623083b
Ismail Bourbie <bourbieismail@gmail.com>
2025-02-06 14:35:27 +0100

    working seeding large dataset

--- database/factories/PackageFactory.php
+++ database/factories/PackageFactory.php
@@ -17,22 +17,22 @@ class PackageFactory extends Factory
     {
         return [
             'uuid' => $this->faker->uuid,
-        'tracking_code' => $this->faker->unique()->bothify('##??##?#?##?#??'),
-        'commune_id' => '1', // TODO: update that with related model
-        'delivery_type_id' => DeliveryType::factory()->create(), // TODO: Update that with related model
-        'status_id' => PackageStatus::factory()->create(), // TODO: Update that with related model
-        'store_id' => Store::factory()->create(),
-        'address' => $this->faker->address,
-        'name' => $this->faker->name,
-        'client_first_name' => $this->faker->firstName,
-        'client_last_name' => $this->faker->lastName,
-        'client_phone' => $this->faker->phoneNumber,
-        'delivery_price' => $this->faker->randomFloat(2, 100, 1000),
-        'free_delivery' => $this->faker->boolean,
-        'partner_delivery_price' => $this->faker->randomNumber(2),
-        'price' => $this->faker->randomFloat(2, 500, 10_000),
-        'price_to_pay' => $this->faker->randomFloat(2, 500, 10_000),
-        'total_price' => $this->faker->randomFloat(2, 500, 10_000),
+            'tracking_code' => $this->faker->unique()->bothify('##??##?#?##?#??'),
+            'commune_id' => '1', // TODO: update that with related model
+            'delivery_type_id' => DeliveryType::factory(), // TODO: Update that with related model
+            'status_id' => PackageStatus::factory(), // TODO: Update that with related model
+            'store_id' => Store::factory(),
+            'address' => $this->faker->address,
+            'name' => $this->faker->name,
+            'client_first_name' => $this->faker->firstName,
+            'client_last_name' => $this->faker->lastName,
+            'client_phone' => $this->faker->phoneNumber,
+            'delivery_price' => $this->faker->randomFloat(2, 100, 1000),
+            'free_delivery' => $this->faker->boolean,
+            'partner_delivery_price' => $this->faker->randomNumber(2),
+            'price' => $this->faker->randomFloat(2, 500, 10_000),
+            'price_to_pay' => $this->faker->randomFloat(2, 500, 10_000),
+            'total_price' => $this->faker->randomFloat(2, 500, 10_000),
         ];
     }
 }
--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -2,13 +2,48 @@
 
 namespace Database\Seeders;
 
+use App\Models\DeliveryType;
+use App\Models\Package;
+use App\Models\PackageStatus;
 use App\Models\Store;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StoreSeeder extends Seeder
 {
     public function run(): void
     {
-        Store::factory()->count(5000)->hasPackages(100)->create();
+
+        $deliveryTypes = DeliveryType::factory()->count(3)->createQuietly();
+        $statuses = PackageStatus::factory()->count(3)->createQuietly();
+
+        $stores = Store::factory()->count(500)->raw();
+
+        $storesChunks = array_chunk($stores, 100);
+        foreach ($storesChunks as $chunk) {
+            DB::table('stores')->insert($chunk);
+        }
+
+        $storesIds = DB::table('stores')->pluck('id');
+        $packages = [];
+        foreach ($storesIds as $index => $storeId) {
+            for ($j = 1; $j <= 100; $j++) {
+                $package = Package::factory()->raw([
+                    'store_id' => $storeId,
+                    'status_id' => $statuses->random()->id,
+                    'delivery_type_id' => $deliveryTypes->random()->id,
+                ]);
+                $packages[] = $package;
+                if ($j * ($index + 1) % 2000 == 0) { // 1000 => 114,465 ms
+                    DB::enableQueryLog();
+                    DB::table('packages')->insert($packages);
+                    dump(count(DB::getQueryLog()));
+
+                    $packages = [];
+                }
+            }
+
+        }
+
     }
 }
