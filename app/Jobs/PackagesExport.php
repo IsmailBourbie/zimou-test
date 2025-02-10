6b0dbf6818d3d6ecaadb7e9c49951daf62a98162
Ismail Bourbie <bourbieismail@gmail.com>
2025-02-10 13:12:16 +0100

    Use Raw SQL instead of query, make the export process faster.

--- app/Jobs/PackagesExport.php
+++ app/Jobs/PackagesExport.php
@@ -4,10 +4,13 @@ namespace App\Jobs;
 
 use App\Models\Package;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Spatie\SimpleExcel\SimpleExcelWriter;
 
 class PackagesExport implements ShouldQueue
@@ -27,34 +30,52 @@ class PackagesExport implements ShouldQueue
      */
     public function handle(): void
     {
-        $path = storage_path('app/exports/packages/' . $this->filename);
+        $start = memory_get_usage();
+        $path = storage_path('app/exports/packages/'.$this->filename);
         $headers = [
             'Tracking Code', 'Store', 'Package name', 'Status',
             'Client', 'Phone', 'Wilaya', 'Commune', 'Delivery Type',
         ];
         $writer = SimpleExcelWriter::create($path)->addHeader($headers);
 
-        Package::query()->select(
-            'id', 'name', 'client_first_name', 'client_last_name',
-            'store_id', 'commune_id', 'delivery_type_id', 'status_id',
-            'tracking_code', 'client_phone'
-        )->with(
-            [
-                'store:id,name,status', 'commune', 'commune.wilaya', 'deliveryType', 'status',
-            ]
-        )->cursor()->each(function ($package) use ($writer) {
-            $writer->addRow([
-                $package->tracking_code,
-                $package->store->name,
-                $package->name,
-                $package->status->name,
-                $package->client_last_name.' '.$package->client_first_name,
-                $package->client_phone,
-                $package->commune->wilaya->name,
-                $package->commune->name,
-                $package->deliveryType->name,
-            ]);
-        });
+        DB::table('packages')
+            ->join('stores', 'packages.store_id', '=', 'stores.id')
+            ->join('communes', 'packages.commune_id', '=', 'communes.id')
+            ->join('wilayas', 'communes.wilaya_id', '=', 'wilayas.id')
+            ->join('delivery_types', 'packages.delivery_type_id', '=', 'delivery_types.id')
+            ->join('package_statuses', 'packages.status_id', '=', 'package_statuses.id')
+            ->select(
+                'packages.id',
+                'packages.name',
+                'packages.client_first_name',
+                'packages.client_last_name',
+                'packages.store_id',
+                'packages.commune_id',
+                'packages.delivery_type_id',
+                'packages.status_id',
+                'packages.tracking_code',
+                'packages.client_phone',
+                'stores.name as store',
+                'communes.name as commune',
+                'wilayas.name as wilaya',
+                'delivery_types.name as delivery_type',
+                'package_statuses.name as status'
+            )->orderBy('id')->cursor()->each(function ($package) use ($writer) {
+                $writer->addRow([
+                    $package->tracking_code,
+                    $package->store,
+                    $package->name,
+                    $package->status,
+                    $package->client_last_name.' '.$package->client_first_name,
+                    $package->client_phone,
+                    $package->wilaya,
+                    $package->commune,
+                    $package->delivery_type,
+                ]);
+            });
+        $end = memory_get_usage();
+
+        Log::info('memory: '. $end - $start);
 
         $writer->close();
 
